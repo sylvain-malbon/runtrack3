f0c494f156dec2c0bd433ad23174b434f5218b8c
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-12-04 13:18:47 +0100

    jobs 05 et 06 mis dans jour01

---
+++ jour01/job06/script.js
@@ -0,0 +1,14 @@
+/* job 05
+* Créez une fonction “afficherjourssemaines”. Cette fonction ne prend pas de paramètre.
+* Créez un tableau de strings “jourssemaines” qui contient l’ensemble des jours de la
+* semaine, du Lundi au Dimanche.Ensuite à l’aide d’une boucle for (for!)Affichez un par
+* un ces jours.
+*/
+
+function afficherjourssemaines() {
+    let joursdelasemaine = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+    for (let i = 0; i < joursdelasemaine.length; i++) {
+        console.log(joursdelasemaine[i]);
+    }
+}
+afficherjourssemaines();
