95f874ead75b9991fd2eeaf1813eb1b0b15e6f4c
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-12-04 13:07:44 +0100

    fonction job04 : année bissextile

--- jour01/job04/script.js
+++ jour01/job04/script.js
@@ -1,5 +1,11 @@
-function hello(message) {
-    console.log(message);
+function bissextile(annee) {
+    // Si l'année est divisible par 4 et pas par 100, ou divisible par 400
+    let result = (iannee % 4 === 0 && annee % 100 !== 0) || annee % 400 === 0
+        // Alors c'est une année bissextile
+        ? `${annee} est une année bissextile`
+        // Sinon ce n'est pas une année bissextile
+        : `${annee} n'est pas une année bissextile`;
+    console.log(result);
 }
 
-hello("Hello JavaScript");
+bissextile(2000);
