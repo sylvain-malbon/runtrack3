96e87fea3a18f291560d8c0eb69a2f7a459c90ba
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-12-04 11:03:16 +0100

    Job01

---
+++ job01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Job01</title>
+<script>
+    alert ("Hello JavaScript");
+</script>
+</head>
+
+<body>
+
+</body>
+
+</html>
