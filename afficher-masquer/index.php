f82d7357ee5b61c0a3780dd82b9da99427b34f77
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-12-05 11:26:10 +0100

    Compteur-js Afficher-masquer

---
+++ afficher-masquer/index.php
@@ -0,0 +1,14 @@
+html
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <script src="./script.js"></script>
+  </head>
+  <body>
+    <div id="boite">Je suis une boîte !</div>
+    <button onclick="toggleBoite()">Afficher/Masquer</button>
+  </body>
+</html>
---
+++ compteur-js/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Compteur JS</title>
+    <script src="script.js"></script>
+</head>
+<body>
+    <p id="compteur">0</p>
+    <button onclick="incrementer()">+1</button>
+    <button onclick="decrementer()">-1</button>
+    <button onclick="reset()">Reset</button>
+</body>
+</html>
