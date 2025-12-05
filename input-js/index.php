76c8c46504d1d31dc8c5f165399386151f22fb2f
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-12-05 11:46:34 +0100

    Correction du titre : afficher-masquer Création : Input-js

---
+++ input-js/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Input JS</title>
+    <script src="script.js"></script>
+</head>
+<body>
+<input type="text" id="prenom" placeholder="Entre ton prénom">
+<button onclick="afficherPrenom()">Valider</button>
+<p id="resultat"></p>
+</body>
+</html>
