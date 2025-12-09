27e74480e33e19ecc44b634f41ea754fb72a3c80
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-12-09 14:41:38 +0100

    idem pour jour03/job01

--- jour03/job01/script.js
+++ jour03/job01/script.js
@@ -1,14 +1,12 @@
 /* Job 01
-Créez une balise <button> et sélectionnez-la dans votre code JavaScript.
-En cliquant sur le bouton, vous devrez faire apparaître le texte sur votre page HTML :
-“Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit,
-ensuite on prie.”
-Ensuite créez un bouton qui servira à cacher tout l’élément html. */
+Adapte le code pour utiliser jQuery */
 
-document.getElementById('show-btn').addEventListener('click', function () {
-    document.getElementById('citation').style.display = 'block';
-});
+$(document).ready(function () {
+    $('#show-btn').on('click', function () {
+        $('#citation').show();
+    });
 
-document.getElementById('hide-btn').addEventListener('click', function () {
-    document.getElementById('main-content').style.display = 'none';
+    $('#hide-btn').on('click', function () {
+        $('#main-content').hide();
+    });
 });
