9c83aa7535d7cca4a56954821a77fe11f25df5d4
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-12-09 15:20:19 +0100

    correction de la consigne :-)

--- jour03/job01/script.js
+++ jour03/job01/script.js
@@ -1,5 +1,9 @@
 /* Job 01
-Adapte le code pour utiliser jQuery */
+Créez une balise <button> et sélectionnez-la dans votre code JavaScript.
+En cliquant sur le bouton, vous devrez faire apparaître le texte sur votre page HTML :
+“Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit,
+ensuite on prie.”
+Ensuite créez un bouton qui servira à cacher tout l’élément html. */
 
 $(document).ready(function () {
     $('#show-btn').on('click', function () {
