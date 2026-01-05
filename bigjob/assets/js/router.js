da34cee8ca3bdb86e6b5fd3cf630c03af9619604
sylvain-malbon <sylvain.malbon@laplateforme.io>
2026-01-05 14:53:44 +0100

    màj router : accès à #register hors-connexion

--- bigjob/assets/js/router.js
+++ bigjob/assets/js/router.js
@@ -1,6 +1,16 @@
 function navigate() {
     const hash = window.location.hash || "#login";
 
+    const publicPages = ["#login", "#register"];
+
+    const user = JSON.parse(sessionStorage.getItem("currentUser"));
+
+    // Protection des pages privées
+    if (!user && !publicPages.includes(hash)) {
+        window.location.hash = "#login";
+        return;
+    }
+
     document.querySelectorAll("section").forEach(sec => sec.classList.add("hidden"));
 
     const page = document.querySelector(hash);
